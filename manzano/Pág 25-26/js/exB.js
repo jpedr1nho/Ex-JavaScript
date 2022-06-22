7d7835a1ea97211559c445c75a6946bc1191ba95
Pedrinho ;) <101263526+jpedr1nho@users.noreply.github.com>
2022-06-22 11:52:54 -0300

    Delete manzano directory

--- manzano/Pág 25-26/js/exB.js
+++
@@ -1,9 +0,0 @@
-let c, f;
-
-alert("Converta a sua temperatura de Fahrenheit para Celcius")
-
-f = parseFloat(prompt("Digite a sua temperatura em Fahrenheit"))
-
-c = ((f-32) * (5/9))
-
-alert("Sua temperatura convertida para Celcius:"+c)
