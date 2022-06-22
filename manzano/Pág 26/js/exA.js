013f13abdf1ef99f3849893218a252c2556bafa4
Pedrinho ;) <101263526+jpedr1nho@users.noreply.github.com>
2022-06-22 11:51:10 -0300

    Add files via upload

---
+++ manzano/Pág 26/js/exA.js
@@ -0,0 +1,14 @@
+let a, b, c, d, p, s;
+
+alert("Resultado de um produto e uma soma");
+
+a = parseInt(prompt("Digitar o valor de A"));
+b = parseInt(prompt("Digitar o valor de B"));
+c = parseInt(prompt("Digitar o valor de C"));
+d = parseInt(prompt("Digitar o valor de D"));
+
+p = a*c
+s = b+d
+
+alert("O produto é de: "+p)
+alert("A soma é de: "+s)
